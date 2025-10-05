34c2ca54a67e44139df309030d34c65b987623ee
Shubham Gupta <shubham0996@gmail.com>
2025-10-05 07:54:13 +0530

    Fixed minor bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -120,14 +120,6 @@ app.get(
   }
 );
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
